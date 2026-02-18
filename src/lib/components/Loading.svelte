e50f25ad2453980f364098181ca37bf0f6569f3e
nerif.tafu <finn.rawles@hotmail.co.uk>
2026-02-18 12:00:02 -0300

    remove everything

--- src/lib/components/Loading.svelte
+++
@@ -1,3 +0,0 @@
-<div class="fixed inset-0 flex items-center justify-center bg-black/80 backdrop-blur-sm">
-  <div class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-</div> 
